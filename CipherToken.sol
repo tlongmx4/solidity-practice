4aad95cb7be004cfc547d320d6115fb2519af183
Timothy Long <tlongmx4@gmail.com>
2025-12-22 22:04:38 -0500

    feat CipherToken v2 (events, errors, decimals)

--- CipherToken.sol
+++ CipherToken.sol
@@ -1,21 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.20;
 
 contract CipherToken {
-    string public name = "CipherToken";
-    string public symbol = "CPT";
+    error InsufficientBalance(uint256 requested, uint256 available);
+    error InvalidRecipient();
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+
+    string public constant name = "CipherToken";
+    string public constant symbol = "CPT";
+    uint8 public constant decimals = 18;
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
     constructor(uint256 initialSupply) {
         totalSupply = initialSupply;
         balanceOf[msg.sender] = initialSupply;
+        emit Transfer(address(0), msg.sender, initialSupply);
     }
 
     function transfer(address toWho, uint256 value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= value, "Insufficient tokens, soz");
-        balanceOf[msg.sender] -= value;
-        balanceOf[toWho] += value;
+        if (toWho == address(0)) revert InvalidRecipient();
+
+        uint256 bal = balanceOf[msg.sender];
+        if (value > bal) revert InsufficientBalance(value, bal);
+
+        unchecked {
+            balanceOf[msg.sender] = bal - value;
+            balanceOf[toWho] += value;
+        }
+
+        emit Transfer(msg.sender, toWho, value);
         return true;
     }
 }
